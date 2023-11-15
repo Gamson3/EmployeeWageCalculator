269b60a816cc5496805ea5f2ed8c2e765e972d31
Gamson3 <gamson3@yahoo.com>
2023-11-16 00:57:50 +0100

    Revert "initial commit."
    This reverts commit 4333e19169fd097336a98e8da0329839acd3ae10.

--- src/EmployeeService.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-public interface EmployeeService {
-    List<Employee> readEmployeeData(String fileName) throws IOException;
-    void readWorkHourData(String fileName, ArrayList<Employee> employees) throws IOException;
-}
--- src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Controller controller = new Controller(new EmployeeServiceImpl()); // Creating an instance of Controller
-        controller.process(args); // Calling the process method on the Controller instance
-    }
-}
